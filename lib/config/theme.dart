8dcf74ac6f6c21e99d6dcdcfc8f92ea1ca085dd5
poojhaakumar0901 <poojhaakumar0901@gmail.com>
2025-07-03 18:02:15 +0530

    Initial Commit

---
+++ lib/config/theme.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import '../utils/theme/text_styles.dart'; 
+
+class AppTheme {
+  static ThemeData lightTheme = ThemeData(
+    brightness: Brightness.light,
+    primarySwatch: Colors.deepPurple,
+    scaffoldBackgroundColor: Colors.white,
+    appBarTheme: const AppBarTheme(
+      backgroundColor: Colors.deepPurple,
+      foregroundColor: Colors.white,
+      elevation: 0,
+    ),
+    textTheme: const TextTheme(
+      headlineSmall: AppTextStyles.headlineSmall,
+      bodyMedium: AppTextStyles.bodyMedium,
+    ),
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: Colors.deepPurple,
+        foregroundColor: Colors.white,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(8)),
+        ),
+      ),
+    ),
+  );
+
+  static ThemeData darkTheme = ThemeData(
+    brightness: Brightness.dark,
+    primarySwatch: Colors.deepPurple,
+    scaffoldBackgroundColor: Colors.black,
+    appBarTheme: const AppBarTheme(
+      backgroundColor: Colors.black,
+      foregroundColor: Colors.white,
+      elevation: 0,
+    ),
+    textTheme: const TextTheme(
+      headlineSmall: AppTextStyles.headlineSmallDark,
+      bodyMedium: AppTextStyles.bodyMediumDark,
+    ),
+  );
+}
---
+++ lib/utils/theme/text_styles.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class AppTextStyles {
+  static const TextStyle hedlineLarge = TextStyle(
+    fontFamily: 'Lexend',
+    fontSize: 26,
+    fontWeight: FontWeight.bold,
+    color: Colors.black,
+  );
+  static const TextStyle headlineSmall = TextStyle(
+    fontFamily: 'Lexend',
+    fontSize: 24,
+    fontWeight: FontWeight.bold,
+    color: Colors.black,
+  );
+
+  static const TextStyle bodyMedium = TextStyle(
+    fontFamily: 'Lexend',
+    fontSize: 16,
+    color: Colors.black87,
+  );
+    static const TextStyle hedlineLargeDark = TextStyle(
+    fontFamily: 'Lexend',
+    fontSize: 26,
+    fontWeight: FontWeight.bold,
+    color: Colors.white,
+  );
+
+  static const TextStyle headlineSmallDark = TextStyle(
+    fontFamily: 'Lexend',
+    fontSize: 24,
+    fontWeight: FontWeight.bold,
+    color: Colors.white,
+  );
+
+  static const TextStyle bodyMediumDark = TextStyle(
+    fontFamily: 'Lexend',
+    fontSize: 16,
+    color: Colors.white70,
+  );
+}
