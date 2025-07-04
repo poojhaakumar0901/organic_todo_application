591972d03a3071f4419da8753f92b4fc0abd1c4e
poojhaakumar0901 <poojhaakumar0901@gmail.com>
2025-07-04 15:04:34 +0530

    after running lefthook

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,6 @@ class MyApp extends StatelessWidget {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Organic Mind',
-      initialRoute: '',
       theme: AppTheme.lightTheme, 
     );
   }
